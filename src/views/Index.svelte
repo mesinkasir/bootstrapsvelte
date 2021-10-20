66ba0c6839c440c3d20cb4fa291ef4cedd96d253
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2021-10-20 21:45:25 +0700

    Update Index.svelte

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -91,7 +91,7 @@
             </a>
         </div>
         
-     <div class="col-12 col-md-12 p-3 text-center">
+     <div class="col-12 col-md-12 p-3 p-md-5 text-center bg-light">
 	     Contact Us : <a href="https://wa.me/6285646104747" class="btn btn-success rounded-pill">Whatsapp</a> or <a href="mailto:axcora@gmail.com" class="btn btn-danger rounded-pill">Email</a>
 		</div>
     
